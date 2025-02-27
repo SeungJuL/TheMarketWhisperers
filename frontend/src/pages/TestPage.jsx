e711614e6a7b589bfcbea264082cf059454c80f8
a-sommer <ava.r.sommer@gmail.com>
2025-02-27 09:53:11 -0500

    Finalized page design

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -38,18 +38,22 @@ const TestPage = ({ setUser }) => {
             {/* Left Section */}
             <div class="w-1/2 flex flex-col justify-between items-center bg-slate-700 rounded-lg border border-slate-500 p-8 ml-2 min-h-[400px]">
                 <h2 class="text-3xl font-bold mt-2">Why Join?</h2>
-                <div class="space-y-6 mb-24 text-center">
+                <div class="space-y-6 mb-14 text-center">
                     <div>
-                        <h3 class="font-semibold text-lg underline">AI-Guided Insights</h3>
-                        <p class="text-white-300">Learn to invest with beginner-friendly advice.</p>
+                      <h3 class="font-semibold text-lg underline">Learn as You Invest</h3>
+                      <p class="text-white-300">Interactive tips and easy explanations help you build confidence step by step.</p>
                     </div>
                     <div>
-                        <h3 class="font-semibold text-lg underline">Clear Visuals</h3>
-                        <p class="text-white-300">Understand stocks through simple charts and metrics.</p>
+                      <h3 class="font-semibold text-lg underline">AI Insights, No Jargon</h3>
+                      <p class="text-white-300">Get plain-English summaries of stock performance, powered by AI.</p>
                     </div>
                     <div>
-                        <h3 class="font-semibold text-lg underline">Track Favorites</h3>
-                        <p class="text-white-300">Build watchlists and stay on top of the market.</p>
+                      <h3 class="font-semibold text-lg underline">Track Stocks with Ease</h3>
+                      <p class="text-white-300">Use our simple watchlist to follow your favorite stocks—no clutter, no confusion.</p>
+                    </div>
+                    <div>
+                      <h3 class="font-semibold text-lg underline">Stay Ahead, Effortlessly</h3>
+                      <p class="text-white-300">See real-time market trends and investor sentiment in just seconds.</p>
                     </div>
                 </div>
             </div>
@@ -59,22 +63,26 @@ const TestPage = ({ setUser }) => {
                 <h2 class="text-3xl font-bold mt-2">Sign Up</h2>
                 <form class="space-y-6 mt-6 w-3/4 max-w-sm">
                     <div class="mb-4">
-                        <label class="block text-sm mb-1">Email</label>
-                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} class="w-full p-2 bg-white-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
+                      <label class="block text-sm mb-1">Email</label>
+                      <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} class="w-full p-2 text-black bg-white-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
                     </div>
                     <div class="mb-4">
-                        <label class="block text-sm mb-1">Password</label>
-                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} class="w-full p-2 bg-white-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
+                      <label class="block text-sm mb-1">Username</label>
+                      <input type="username" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)} class="w-full p-2 text-black bg-white-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
                     </div>
                     <div class="mb-4">
-                        <label class="block text-sm mb-1">Confirm Password</label>
-                        <input type="password" placeholder="Confirm Password" class="w-full p-2 bg-white-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
+                      <label class="block text-sm mb-1">Password</label>
+                      <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} class="w-full p-2 text-black bg-white-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
+                    </div>
+                    <div class="mb-4">
+                      <label class="block text-sm mb-1">Confirm Password</label>
+                      <input type="password" placeholder="Confirm Password" required class="w-full p-2 text-black bg-white-300 rounded focus:outline-none focus:ring-2 focus:ring-purple-400" />
                     </div>
                     <button onClick={handleSignup} class="w-full p-2 bg-slate-900 hover:bg-blue-800 rounded text-white font-semibold">Sign Up</button>
                 </form>
 
-                <h3 class="text-lg mb-4">Already signed up?
-                    <a href="#" class="text-blue-400">Login</a>
+                <h3 class="text-lg mb-4">Already signed up? 
+                    <a href="#" class="text-blue-400"> Login</a>
                 </h3>
             </div>
         </div>       
