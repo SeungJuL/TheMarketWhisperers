ae69109375efcbb70ef23195f9665f0133738362
SeungJuL <seungju.lee@ufl.edu>
2025-03-07 15:45:25 -0500

    Added ai api

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from controller.user_controller import User
 from blueprint.user import user_blueprint
 from blueprint.stock import stock_blueprint
+from blueprint.ai import ai_blueprint
 import os
 
 app = Flask(__name__)
@@ -28,6 +29,7 @@ def unauthorized():
 # blue print
 app.register_blueprint(user_blueprint, url_prefix='/user')
 app.register_blueprint(stock_blueprint, url_prefix='/stock')
+app.register_blueprint(ai_blueprint, url_prefix='/ai')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080', debug=True)
