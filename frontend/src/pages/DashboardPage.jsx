9ce0fb2ae3200432d02a57e6f3adfd39df55d63c
a-sommer <ava.r.sommer@gmail.com>
2025-04-10 14:18:25 -0400

    Implemented stock chart

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { EyeIcon, HomeIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import PageWrapper from '../components/PageWrapper';
 import Navbar from '../components/Navbar';
+import StockChart from "../components/StockChart";
 
 
 const DashboardPage = () => {
@@ -14,10 +15,27 @@ const DashboardPage = () => {
   const [stockData, setStockData] = useState('');
   const [isVisible, setIsVisible] = useState(false);
   
+  // Function to clean and parse dates
+  const parseDate = (dateStr) => {
+    // Strip out the weekday name (e.g., 'Thu,' or 'Mon,')
+    const cleanedDateStr = dateStr.replace(/^[A-Za-z]{3},\s/, '');
+
+    // Debugging
+    //console.log('Cleaned data/: $(cleanedDataStr');
+
+    const dateObj = new Date(cleanedDateStr);
+
+    // Check if the date is valid
+    if (isNaN(dateObj.getTime())) {
+      console.error(`Invalid date: ${dateStr}`);
+      return null; // Return null if invalid
+    }
+
+    return dateObj;
+  };
 
   const getStockData = async (stockName) => {
 
-    //e.preventDefault();
     setMessage("");  // Clear messages before a new attempt
     setError("");
 
@@ -27,38 +45,62 @@ const DashboardPage = () => {
       });
 
       const stocks = await response.json();
-      console.log("API Response:", stocks); // Debugging
+      //console.log("API Response:", stocks);
 
       if (stocks.success) {
 
-        console.log("GOT:" + stocks.data[0]['1. symbol']);  // ✅ Show success message
+        //console.log("GOT:" + stocks.data[0]['1. symbol']);  // ✅ Show success message
 
         // Stock price
         const price_response = await fetch("http://localhost:8080/stock/" + stocks.data[0]['1. symbol'] + "/price?asset_symbol=" + stocks.data[0]['1. symbol'], {
           method: "GET"
         });
 
-        const price = await price_response.json();
-        console.log("Price API Response:", price);
-
         // Stock metrics
         const si_response = await fetch("http://localhost:8080/stock/" + stocks.data[0]['1. symbol'] + "/info", {
           method: "GET"
         });
 
-        const stockInfo = await si_response.json();
-        console.log("Stock Info API Response:", stockInfo);
-
         // Historical data 
-        const his_response = await fetch("http://localhost:8080/stock/" + stocks.data[0]['1. symbol'] + "/history", {
+        const hist_response = await fetch("http://localhost:8080/stock/" + stocks.data[0]['1. symbol'] + "/history", {
           method: "GET"
         });
+        
+        const price = await price_response.json();
+        //console.log("Price API Response:", price);
 
-        const historicalInfo = await his_response.json();
-        console.log("Historial Data API Response:", historicalInfo);
+        const stockInfo = await si_response.json();
+        //console.log("Stock Info API Response:", stockInfo);
+
+        const historicalInfo = await hist_response.json();
+        //console.log("Historial Data API Response:", historicalInfo);
 
         // Use length - 2 to get latest Close price
         const closeVal = historicalInfo.data[Object.keys(historicalInfo.data).length - 2]['Close'];
+
+        // Extract dates and close values for chart
+        const rawDates = historicalInfo.data.map(item => item.Date);
+        const chartLabels = [];
+        const chartClose = [];
+        const chartHighs = [];
+        const chartLows = [];
+
+        for (let i = 0; i < rawDates.length - 1; i++) {
+          const dateStr = rawDates[i];
+
+          // Use the parseDate function to clean and parse the date
+          const dateObj = parseDate(dateStr);
+
+          if (!dateObj) continue; // Skip if date is invalid
+
+          // Format the date as "Month Day" (e.g., "March 10")
+          const formattedDate = new Intl.DateTimeFormat('en-US', { month: 'long', day: '2-digit', timeZone: 'UTC' }).format(dateObj);
+          
+          chartLabels.push(formattedDate);
+          chartClose.push(historicalInfo.data[i]['Close']);
+          chartHighs.push(historicalInfo.data[i]['High']);
+          chartLows.push(historicalInfo.data[i]['Low']);
+        }
 
         const calcChange = (((price.data['price'] - closeVal) / closeVal) * 100);
 
@@ -74,6 +116,10 @@ const DashboardPage = () => {
           symbol: stocks.data[0]['1. symbol'],
           price: price.data['price'],
           change: calcChange,
+          chartLabels,
+          chartClose,
+          chartHighs,
+          chartLows,
           metrics: {
             weekHigh: stockInfo.data['52_week_high'],
             weekLow: stockInfo.data['52_week_low'],
@@ -85,30 +131,29 @@ const DashboardPage = () => {
             dividendYield: stockInfo.data['dividend_yield'],
             beta: stockInfo.data['beta'],
             debtToEquity: stockInfo.data['debt_to_equity'],
-            peRatio: stockInfo.data['pe_ratio']
+            peRatio: stockInfo.data['pe_ratio'],
           }}
         
-          setIsVisible(true); // Display Stock Header & Metrics Data
-
-        return stockData;
+          setIsVisible(true); // Display Stock Header, Metrics Data, & Stock Chart
+          return stockData;
 
       } else {
         setError(`Fetch stock failed! ${stocks.message}`);  // ✅ Show error message
       }
     } catch (error) {
-      console.error("Login Error:", error);
+      console.error("Error:", error);
       setError("Failed to connect to the server.");
     }
   };
 
   const MetricCard = ({ label, value, tooltip }) => (
-    <div className="bg-slate-700 p-4 rounded-lg relative group">
+    <div className="bg-slate-700 p-3 rounded-lg relative group ">
       <div className="flex items-center mb-2">
         <span className="text-slate-400 text-sm">{label}</span>
         {tooltip && (
           <div className="ml-2">
             <span className="text-slate-400 cursor-help">ⓘ</span>
-            <div className="absolute hidden group-hover:block bg-slate-800 text-white p-2 rounded-md text-sm z-10 w-48">
+            <div className="absolute hidden group-hover:block bg-slate-800 text-white p-2 rounded-md text-sm w-48">
               {tooltip}
             </div>
           </div>
@@ -121,6 +166,7 @@ const DashboardPage = () => {
   return (
     <PageWrapper>
       <div className="flex flex-col items-center justify-center flex-grow px-4 md:px-8 mt-20 mb-20">
+      
         {/* Top Navigation */}
         <div className="bg-transparent p-4 flex items-center justify-center z-40">
           <div className="relative">
@@ -157,53 +203,61 @@ const DashboardPage = () => {
 
             {/* Metrics Grid */}
             {isVisible &&
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-              <MetricCard
-                label="52 Week High/Low"
-                value={parseFloat(stockData.metrics.weekHigh).toFixed(2) + ' / ' + parseFloat(stockData.metrics.weekLow).toFixed(2)}
-                tooltip="The highest and lowest stock price over the past 52 weeks"
-              />
-              <MetricCard
-                label="Earnings Per Share"
-                value={stockData.metrics.eps}
-                tooltip="Company's profit divided by outstanding shares"
-              />
-              <MetricCard
-                label="Revenue Growth"
-                value={parseFloat(stockData.metrics.revenueGrowth).toFixed(2) + '%'}
-                tooltip="Year-over-year revenue growth percentage"
-              />
-              <MetricCard
-                label="Market Cap"
-                value={stockData.metrics.marketCap}
-                tooltip="Total market value of company's outstanding shares"
-              />
-              <MetricCard
-                label="Volume/Average Volume"
-                value={`${Number(stockData.metrics.volume).toLocaleString()} / ${Number(stockData.metrics.avgVolume).toLocaleString()}`}
-                tooltip="Today's trading volume vs 3-month average"
-              />
-              <MetricCard
-                label="Dividend Yield"
-                value={stockData.metrics.dividendYield}
-                tooltip="Annual dividend payments relative to stock price"
-              />
-              <MetricCard
-                label="Beta"
-                value={parseFloat(stockData.metrics.beta).toFixed(2)}
-                tooltip="Stock's volatility compared to the market"
-              />
-              <MetricCard
-                label="Debt-to-Equity Ratio"
-                value={parseFloat(stockData.metrics.debtToEquity).toFixed(2)}
-                tooltip="Total liabilities divided by shareholder equity"
-              />
-              <MetricCard
-                label="P/E Ratio"
-                value={parseFloat(stockData.metrics.peRatio).toFixed(2)}
-                tooltip="Stock price relative to earnings per share"
-              />
-            </div>}
+              <div className="flex flex-col md:flex-row gap-6 mb-6 w-full">
+                {/* Metric Cards */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:w-1/2">
+                  <MetricCard
+                    label="52 Week High/Low"
+                    value={parseFloat(stockData.metrics.weekHigh).toFixed(2) + ' / ' + parseFloat(stockData.metrics.weekLow).toFixed(2)}
+                    tooltip="The highest and lowest stock price over the past 52 weeks"
+                  />
+                  <MetricCard
+                    label="Earnings Per Share"
+                    value={stockData.metrics.eps}
+                    tooltip="Company's profit divided by outstanding shares"
+                  />
+                  <MetricCard
+                    label="Revenue Growth"
+                    value={parseFloat(stockData.metrics.revenueGrowth).toFixed(2) + '%'}
+                    tooltip="Year-over-year revenue growth percentage"
+                  />
+                  <MetricCard
+                    label="Market Cap"
+                    value={stockData.metrics.marketCap}
+                    tooltip="Total market value of company's outstanding shares"
+                  />
+                  <MetricCard
+                    label="Volume/Average Volume"
+                    value={`${Number(stockData.metrics.volume).toLocaleString()} / ${Number(stockData.metrics.avgVolume).toLocaleString()}`}
+                    tooltip="Today's trading volume vs 3-month average"
+                  />
+                  <MetricCard
+                    label="Dividend Yield"
+                    value={stockData.metrics.dividendYield}
+                    tooltip="Annual dividend payments relative to stock price"
+                  />
+                  <MetricCard
+                    label="Beta"
+                    value={parseFloat(stockData.metrics.beta).toFixed(2)}
+                    tooltip="Stock's volatility compared to the market"
+                  />
+                  <MetricCard
+                    label="Debt-to-Equity Ratio"
+                    value={parseFloat(stockData.metrics.debtToEquity).toFixed(2)}
+                    tooltip="Total liabilities divided by shareholder equity"
+                  />
+                  <MetricCard
+                    label="P/E Ratio"
+                    value={parseFloat(stockData.metrics.peRatio).toFixed(2)}
+                    tooltip="Stock price relative to earnings per share"
+                  />
+                </div>
+
+                {/* Stock Chart */}
+                <div className="md:w-1/2 flex justify-center items-center">
+                  <StockChart historicalInfo={stockData} />
+                </div>
+              </div>}          
           </div>
 
           {/* Tabs */}
